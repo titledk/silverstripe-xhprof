d4c1189fefd631decd1b32ed848e018fc8d8322b
Anselm Christophersen <ac@anselm.dk>
2012-08-07 11:09:51 +0200

    Only profile if get parameter "xhprof" is set
    Inspired by Wilr: https://gist.github.com/3271377

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,11 +1,13 @@
 <?php
-if (extension_loaded('xhprof')) {
-    $profiler_namespace = 'myapp';  // namespace for your application
-    $xhprof_data = xhprof_disable();
-    $xhprof_runs = new XHProfRuns_Default();
-    $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
- 
-    // url to the XHProf UI libraries (change the host name and path)
-    $profiler_url = sprintf('/xhprof/xhprof_html/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
-    echo '<a href="'. $profiler_url .'" target="profile">Profiler output</a>';
+if (isset($_GET["xhprof"]) &&  extension_loaded('xhprof')) {
+  $profiler_namespace = 'myapp';  // namespace for your application
+  $xhprof_data = xhprof_disable();
+  $xhprof_runs = new XHProfRuns_Default();
+  $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace);
+
+  // url to the XHProf UI libraries (change the host name and path)
+  $profiler_url = sprintf('/xhprof/xhprof_html/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
+
+	echo '<a href="'. $profiler_url .'" target="profile">Profiler output</a>';	
+   
 }
